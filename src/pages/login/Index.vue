badf9a61956ec86ec95ec2109a085189b798ddfd
Violet <1054197367@qq.com>
2022-07-03 19:20:48 +0800

    bugfix

--- src/pages/login/Index.vue
+++ src/pages/login/Index.vue
@@ -23,9 +23,9 @@
 
                     <div class="flex items-center justify-between">
                         <div class="flex items-center">
-                            <label for="remember-me" class="ml-2 block text-sm text-gray-900">自动登录</label>
                             <input id="remember-me" v-model="rememberMe" type="checkbox"
                                 class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                            <label for="remember-me" class="ml-2 block text-sm text-gray-900">自动登录</label>
                         </div>
 
                         <div class="text-sm">
