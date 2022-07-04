d7e1545caa4c77eeee5f7a0d62dc0456a1f12255
Violet <1054197367@qq.com>
2022-07-04 22:10:47 +0800

    底部播放器第一个版本

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,14 +7,21 @@
         </div>
 
         <!-- 这是一个底部的固定播放器 -->
-        <div class="w-screen h-16 bg-pink-300">
-            Fixed Bottom Player
+        <div class="h-28 px-2 flex items-center bg-white border-t border-slate-100">
+            <img :src="avatar" alt="" class="w-14 h-14 object-cover rounded-full">
+            <div class="ml-2 line-clamp-1 text-sm font-extrabold">Departures~巴拉巴拉巴拉巴拉啦aaaaaaa啊</div>
+            <div class="w-20 flex justify-between ml-2">
+                <play-icon class="w-6 h-6 text-black" />
+                <menu-alt3-icon class="w-6 h-6 text-black" />
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang='ts'>
-import { onMounted, onUpdated } from 'vue'
+import { MenuAlt3Icon, PlayIcon } from '@heroicons/vue/solid'
+import { onMounted, onUpdated, ref } from 'vue'
+const avatar = ref('http://big-bird.buzz:9519/music-station/avatar.jpg')
 
 /**
  * 获取浏览器可视窗口的大小，主要是作用域类似于Safari浏览器底部工具栏的问题（Safari浏览器可以把工具栏设置到底部）
--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -70,7 +70,6 @@ const addData = () => {
 
 
 onMounted(() => {
-
     addData()
 })
 
